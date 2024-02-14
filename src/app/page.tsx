eca66959118e2582f8e54d93212111098a60a07a
Ivan B <sensenabi@gmail.com>
2024-02-14 11:23:41 -0500

    edge runtime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,13 @@ export default function Home() {
       <div className="flex flex-col px-4">
         <a className="text-blue-600" href="/far/unstable-cache">unstable_cache</a>
         <a className="text-blue-600" href="/far/unstable-cache/streaming">unstable_cache + streaming</a>
+        <a className="text-blue-600" href="/far/unstable-cache/streaming-edge">unstable_cache + streaming (vercel edge runtime)</a>
       </div>
       <p>Cache Tests Close (25ms simulated lantency)</p>
       <div className="flex flex-col px-4">
         <a className="text-blue-600" href="/close/unstable-cache">unstable_cache</a>
         <a className="text-blue-600" href="/close/unstable-cache/streaming">unstable_cache + streaming</a>
+        <a className="text-blue-600" href="/close/unstable-cache/streaming-edge">unstable_cache + streaming (vercel edge runtime)</a>
       </div>
     </main>
   );
